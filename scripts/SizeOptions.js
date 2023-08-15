3f1c0db422a2a2f1debf55034d23a0210dd71855
Helen Esterman <helenesterman99@gmail.com>
2023-08-15 12:59:05 -0400

    swapped forof loop in my metal, size and style option modules for .map method to create html strings of the radio buttons in divs

--- scripts/SizeOptions.js
+++ scripts/SizeOptions.js
@@ -1,9 +1,12 @@
 export const SizeOptions = async () => {
     const response = await fetch("http://localhost:8088/sizes")
     const sizeOptions = await response.json()
-    let sizeChoices = ""
-    for (const size of sizeOptions) {
-        sizeChoices += `<input type="radio" name="sizes" value="${size.id}"/>${size.carets}`
-    }
-    return sizeChoices
+    const sizeChoices = sizeOptions.map(
+        (size) => {
+            return `<div><input type="radio" name="sizes" value="${size.id}"/>${size.carets}</div>`
+        }
+    ) 
+    const finalSizeHtml = sizeChoices.join(" ")
+
+    return finalSizeHtml
 }
