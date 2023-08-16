2a905fec3139ee2a95e53fd673d992049bb7a225
Helen Esterman <helenesterman99@gmail.com>
2023-08-16 12:20:54 -0400

    updated Orders function fetch link to include all forgein keys in order to calculate the price of each order

--- scripts/OrderList.js
+++ scripts/OrderList.js
@@ -1,9 +1,10 @@
 export const Orders = async () =>{
-    const response = await fetch("http://localhost:8088/orders")
+    const response = await fetch("http://localhost:8088/orders?_expand=metal&_expand=style&_expand=size")
     const orders = await response.json()
     const orderList = orders.map(
         (order) =>{
-            return `<div>Order #${order.id}</div>`
+            const orderPrice = order.metal.price + order.style.price + order.size.price
+            return `<div>Order #${order.id} cost $${orderPrice}</div>`
         }
     )
     const finalOrderList = orderList.join(" ")
